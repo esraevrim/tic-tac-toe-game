335d44741513f632d31f5aa00359d0a7546bacb9
Esra Evrim Özer <159051084+esraevrim@users.noreply.github.com>
2025-01-22 00:42:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe newGame = new TicTacToe();
+            int row;
+            int column;
+            int currentPlayer=1;
+            int moveCount = 0;
+
+            while (newGame.CheckWinner() == 0 && moveCount<9)
+            {
+                newGame.PrintBoard();
+                if (moveCount % 2 == 0)
+                {
+                    currentPlayer = 1;
+                }
+                else if (moveCount % 2 !=0)
+                {
+                    currentPlayer = 2;
+                }
+                Console.WriteLine($"\nPlayer {currentPlayer} enter the row you want to play: ");
+                row = int.Parse(Console.ReadLine());
+                Console.WriteLine($"\nPlayer {currentPlayer} enter the column you want to play: ");
+                column = int.Parse(Console.ReadLine());
+                newGame.MakeMove(row, column, currentPlayer);
+                moveCount++;
+            }
+            int winner = newGame.CheckWinner();
+            if (winner != 0) {
+                Console.WriteLine($"Player{winner} won!");
+            }
+            else
+            {
+                Console.WriteLine("Its a draw!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Game
+{
+    class TicTacToe
+    {
+        private int[,] board;  
+        public TicTacToe()
+        {
+            board = new int[3, 3]
+            {
+            { 0, 0, 0 },
+            { 0, 0, 0 },
+            { 0, 0, 0 }
+            };
+        }
+        public void PrintBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == 0)
+                        Console.Write(" . ");
+                    else if (board[i, j] == 1)
+                        Console.Write(" X ");
+                    else
+                        Console.Write(" O ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public bool MakeMove(int x, int y, int player)
+        {
+            if (x >= 0 && x < 3 && y >= 0 && y < 3)
+            {
+                if (board[x, y] == 0)
+                {
+                    board[x, y] = player;
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("This cell is already occupied. Try again.");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid move. Coordinates must be between 0 and 2.");
+                return false;
+            }
+        }
+
+        public int CheckWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != 0)
+                    return board[i, 0];
+            }
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[0, j] == board[1, j] && board[1, j] == board[2, j] && board[0, j] != 0)
+                    return board[0, j];
+            }
+
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != 0)
+                return board[0, 0];
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != 0)
+                return board[0, 2];
+
+            return 0;
+        }
+    }
+}
